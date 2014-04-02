c67c438a502a7c742ca7af9a5453ec0494713d0d
Fraser Scott <admin@privacythroughchoice.org>
2014-04-02 11:58:46 +0100

    rebrand

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # Destroy current image
 
 # Build new image
-rm -rf thunr_packer/cookbooks
-cd thunr_chef_repo
-berks install --path ../thunr_packer/cookbooks
-cd ../thunr_packer
-packer build thunr-site.json
+rm -rf packer/cookbooks
+cd chef_repo
+berks install --path ../packer/cookbooks
+cd ../packer
+packer build site.json
